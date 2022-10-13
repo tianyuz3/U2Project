bad12618b25d92beafca0523a8a8e86e4ec416eb
tianyuz3 <tzhao9389@bths.edu>
2022-10-12 21:15:53 -0400

    runner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner c= new Scanner(System.in);
+        System.out.println("PLease enter the first coordinate pair!: ");
+        int cX=c.nextInt();
+        int cY=c.nextInt();
+        Scanner d=new Scanner(System.in);
+        System.out.println("Please enter the second coordinate pair!: ");
+        int c3=d.nextInt();
+        int c4=d.nextInt();
+
+        LinearEquation coordinate=new LinearEquation(cX,cY,c3,c4);
+        System.out.println(coordinate.toString());
+
+        Scanner x=new Scanner(System.in);
+        System.out.println("Please enter an x-value: ");
+        double x3=x.nextDouble();
+        LinearEquation xCoordinate=new LinearEquation(x3);
+        System.out.println(xCoordinate.calcCP());
     }
 }
